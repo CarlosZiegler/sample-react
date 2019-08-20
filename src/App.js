6159bb549c9f49d3afa50085353cc7d57a4f11db
CarlosZiegler <carlos.ziegler@gmail.com>
2019-08-20 10:48:07 +0200

    Routes and Product Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Routes from './routes';
 
 
 import Header from './components/Header';
-import Main from './pages/main'
+
 
 import './styles.css';
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes/>
     </div>
   );
 }
